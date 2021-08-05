6adfb629e9cefccd3d762f82865e8162e55ddf15
Neil <neiliss@gmail.com>
2021-08-04 20:18:52 -0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld
+    msg="| NEIL ISSEROW – RESUME | CALIFORNIA, USA | NEIL@ISSEROW.COM | 415-515-0222 |"
+  />
+</template>
+
+<script>
+import HelloWorldVue from "./components/HelloWorld.vue";
+export default {
+  name: "App",
+  components: {
+    HelloWorld: HelloWorldVue,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,111 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <p>
+      I have spent more than 2 decades in various senior positions in the IT
+      industry in South Africa, Australia and the USA. I have had broad
+      management, consulting, sales, and technical experience in this time with
+      global companies such as Nampak, Microsoft and VMware in a leadership
+      position. From the data center to the cloud, containers, automation and
+      infrastructure as code, I have tried to keep my experience varied and
+      customer centric, to the constant changes in our world. I have had
+      extensive and incredibly varied experience in my career, and thrive as a
+      “motivated, passionate and results driven individual”, who loves his
+      customer and always does the right thing.
+    </p>
+    <h3>Career Summary</h3>
+    <ul>
+      <li>USA</li>
+      <li>
+        - 2021-Present: Manager, Tanzu Customer Success West US @ VMware
+        <p></p>
+        - 2020-Present: Tanzu Customer Success Manager – West @ VMware
+        <p></p>
+        - 2014-2020: Staff TAM, Team Lead, CTO Ambassador @ VMware
+      </li>
+    </ul>
+    <h3>Essential Links</h3>
+    <ul>
+      <li>
+        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
+      </li>
+      <li>
+        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
+          >Forum</a
+        >
+      </li>
+      <li>
+        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
+          >Community Chat</a
+        >
+      </li>
+      <li>
+        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
+          >Twitter</a
+        >
+      </li>
+      <li>
+        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
+      </li>
+    </ul>
+    <h3>Ecosystem</h3>
+    <ul>
+      <li>
+        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
+          >vue-router</a
+        >
+      </li>
+      <li>
+        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-devtools#vue-devtools"
+          target="_blank"
+          rel="noopener"
+          >vue-devtools</a
+        >
+      </li>
+      <li>
+        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
+          >vue-loader</a
+        >
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/awesome-vue"
+          target="_blank"
+          rel="noopener"
+          >awesome-vue</a
+        >
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
